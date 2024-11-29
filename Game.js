54cfa44edaaa3e3d15d4276d4d3bd2eac52c587f
ZeroDeDragons <162035031+ZeroDeDragons@users.noreply.github.com>
2024-11-29 18:02:31 +0000

    Update Game.js

--- Game.js
+++ Game.js
@@ -138,7 +138,7 @@ function create() {
     // Certifique-se de adicionar o joystick corretamente
     joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
         x: 100,  // Posição inicial do joystick
-        y: 100,
+        y: 0,
         radius: 100,
         enable: true
     });
